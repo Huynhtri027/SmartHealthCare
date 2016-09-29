1406dcfeec02d81d79385522d3a1437a80ebbb4c
Raj Pratim <rajpratim1234@gmail.com>
2016-09-29 17:58:04 +0530

    Doccumentation added

--- CompleteHealthCare/app/src/main/java/com/example/rajpratim/completehealthcare/MainActivity.java
+++ CompleteHealthCare/app/src/main/java/com/example/rajpratim/completehealthcare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rajpratim.completehealthcare;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,6 +11,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+
+import com.example.rajpratim.completehealthcare.Caloriemeter.ActivityTracker;
+import com.example.rajpratim.completehealthcare.Food_Tracker.Food_tracker;
+import com.example.rajpratim.completehealthcare.R;
+
 
 public class MainActivity extends AppCompatActivity {
 public ImageButton imgbtn;
